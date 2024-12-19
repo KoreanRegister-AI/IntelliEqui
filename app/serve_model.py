2704bb34daf64f97ad55a186fbcf4791e7951fe5
MJK <160573101+MJMJKimm@users.noreply.github.com>
2024-12-19 12:52:06 +0900

    Update serve_model.py

--- app/serve_model.py
+++ app/serve_model.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+# 모델 로드
+with open("saved_model/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.get("/")
+def home():
+    return {"status": "Model server is running"}
+
+@app.get("/predict/")
+def predict(input_value: float):
+    prediction = model.predict([[input_value]])
+    return {"prediction": prediction.tolist()}
 
